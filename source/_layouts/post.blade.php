ffcfbe836b7bc7dc76838fba6ddb86047453a13d
Danny Holloran <hello@danholloran.me>
2018-07-07 09:58:38 -0500

    Layout wip

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -1,6 +1,6 @@
 @extends('_layouts.master')
 
 <h1>{{ $page->title }}</h1>
-<p>By {{ $page->author }} • {{ date('F j, Y', $page->date) }}</p>
+{{-- <p>By {{ $page->author }} • {{ date('F j, Y', $page->date) }}</p> --}}
 
 @yield('content')
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,27 @@
+---
+pagination:
+  collection: posts
+  perPage: 10
+---
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+  <p>Total of {{ $posts->count() }} posts</p>
+
+<ul>
+@foreach ($pagination->items as $post)
+    <li>
+      @if($post->date)
+        <span>{{ date('j M Y',  strtotime($post->date)) }}</span>
+      @endif
+
+      <span>{{ $post->author }}</span>
+      <h2>
+        <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
+      </h2>
+
+      <a href="{{ $post->getUrl() }}">Read More</a>
+    </li>
+@endforeach
+</ul>
 @endsection
