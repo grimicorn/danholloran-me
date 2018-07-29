95143cf941f08fef6bdaf435d1d09b81c8397363
Danny Holloran <hello@danholloran.me>
2018-07-29 09:50:05 -0500

    Fixes code not highlighting with Turbolinks

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -3,3 +3,10 @@ import HighlightJs from "highlight.js";
 
 Turbolinks.start();
 HighlightJs.initHighlightingOnLoad();
+
+/**
+ * Highlight code when Turbolinks loads a page.
+ */
+document.addEventListener("turbolinks:load", function() {
+    document.querySelectorAll("pre code").forEach(HighlightJs.highlightBlock);
+});
