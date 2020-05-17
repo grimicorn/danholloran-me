a063ded5f4f8c1dd79d7610f350419541d243e56
Danny Holloran <hello@danholloran.me>
2020-05-17 09:54:49 -0500

    Adds instagram and youtube

--- source/_nav/social.blade.php
+++ source/_nav/social.blade.php
@@ -2,35 +2,50 @@
     {{-- Icons are from https://icongr.am/material --}}
     <ul class="flex items-center">
         <li class="mr-2">
-            <a href="https://twitter.com/dholloran" title="Twitter" class="h-6 block text-indigo-700">
+            <a href="https://twitter.com/dholloran" title="Twitter" class="block h-6 text-indigo-700">
                 @include('_partials.icon-twitter')
                 <span class="sr-only">Twitter</span>
             </a>
         </li>
 
         <li class="mr-2">
-            <a href="https://github.com/dholloran" title="GitHub" class="h-6 block text-indigo-700">
+            <a href="https://github.com/dholloran" title="GitHub" class="block h-6 text-indigo-700">
                 @include('_partials.icon-github')
                 <span class="sr-only">GitHub</span>
             </a>
         </li>
 
         <li class="mr-2">
-            <a href="https://www.linkedin.com/in/dan-holloran/" title="LinkedIn" class="h-6 block text-indigo-700">
+            <a href="https://www.linkedin.com/in/dan-holloran/" title="LinkedIn" class="block h-6 text-indigo-700">
                 @include('_partials.icon-linkedin')
                 <span class="sr-only">LinkedIn</span>
             </a>
         </li>
 
         <li class="mr-2">
-            <a href="/feed.xml" title="RSS" class="h-6 block text-indigo-700">
+            <a href="https://www.youtube.com/channel/UCm0poVascsWEeiAEkjsfVjQ" title="YouTube"
+                class="block h-6 text-indigo-700">
+                @include('_partials.icon-youtube')
+                <span class="sr-only">YouTube</span>
+            </a>
+        </li>
+
+        <li class="mr-2">
+            <a href="https://www.instagram.com/dholloran2/" title="Instagram" class="block h-6 text-indigo-700">
+                @include('_partials.icon-instagram')
+                <span class="sr-only">Instagram</span>
+            </a>
+        </li>
+
+        <li class="mr-2">
+            <a href="/feed.xml" title="RSS" class="block h-6 text-indigo-700">
                 @include('_partials.icon-rss')
                 <span class="sr-only">RSS</span>
             </a>
         </li>
 
         <li>
-            <a href="https://gitlab.com/DHolloran" title="Gitlab" class="h-6 block text-indigo-700">
+            <a href="https://gitlab.com/DHolloran" title="Gitlab" class="block h-6 text-indigo-700">
                 @include('_partials.icon-gitlab')
                 <span class="sr-only">Gitlab</span>
             </a>
