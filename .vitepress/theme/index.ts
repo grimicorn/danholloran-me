bd7aa215521d4225ca092620b23c5ee725f21ee4
Danny Holloran <DTHolloran@Gmail.com>
2023-05-14 11:36:38 -0500

    Initial vite setup

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,14 +2,11 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
+import BaseLayout from './components/BaseLayout.vue'
 
 export default {
   ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: BaseLayout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
---
+++ posts/if-youve-ever-wondered.md
@@ -0,0 +1,11 @@
+---
+extends: _layouts.post
+section: content
+title: Experimenting With Laravel and Vue.js
+date: 2015-12-05
+categories: []
+published: true
+---
+
+**This is what I do all day long.**
+![](/assets/img/VhlQKh.jpg)
