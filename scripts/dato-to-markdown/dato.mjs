2b9d84e96c1d502de9632bb4680ae501b1443b09
Danny Holloran <hello@danholloran.me>
2023-05-21 15:39:43 -0500

    Adds dato to markdown

--- scripts/dato-to-markdown/dato.mjs
+++ scripts/dato-to-markdown/dato.mjs
@@ -1,3 +1,5 @@
+import { success, danger, info } from "./cli.mjs";
+
 const ResponsiveImageAttributes = `fragment ResponsiveImageAttributes on ResponsiveImage {
   src
   width
@@ -62,223 +64,24 @@ const SiteAttributes = `fragment SiteAttributes on Site {
 }`;
 
 export const getModels = async () => {
-  return {
-    posts: [
-      {
-        id: "116306000",
-        title: "Test Post 1",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:43:16+01:00",
-        slug: "test-post-1",
-        content: "## h2\n\n* Some content\n* Some more content",
-        tags: [],
-        seo: null,
-      },
-      {
-        id: "116306016",
-        title: "Test Post 8",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-8",
-        content: "Some content",
-        tags: [{ id: "116306017", title: "Front End" }],
-        seo: null,
-      },
-      {
-        id: "116306015",
-        title: "Test Post  7",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-7",
-        content: "Some content",
-        tags: [],
-        seo: null,
-      },
-      {
-        id: "116306014",
-        title: "Test Post 6",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-6",
-        content: "Some content",
-        tags: [{ id: "116306017", title: "Front End" }],
-        seo: null,
-      },
-      {
-        id: "116306013",
-        title: "Test Post 5",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-5",
-        content: "Some content",
-        tags: [],
-        seo: null,
-      },
-      {
-        id: "116306012",
-        title: "Test Post 4",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-4",
-        content: "Some content",
-        tags: [],
-        seo: null,
-      },
-      {
-        id: "116306010",
-        title: "Test Post 2",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-2",
-        content: "Some content",
-        tags: [{ id: "116306017", title: "Front End" }],
-        seo: null,
-      },
-      {
-        id: "116306011",
-        title: "Test Post 3",
-        image: {
-          responsiveImage: {
-            src: "https://www.datocms-assets.com/101601/1684691116-2400x1400.png?crop=faces&w=690",
-            width: 690,
-            height: 402,
-            alt: null,
-            title: null,
-            base64:
-              "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgFCgoFBQwFBQUFBREJCgUMFxMZGBYTFhUaHysjGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLBQUFEAUFEC8cFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA4AGAMBIgACEQEDEQH/xAAVAAEBAAAAAAAAAAAAAAAAAAAABv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/xAAVAQEBAAAAAAAAAAAAAAAAAAACAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/ALwARAEn/9k=",
-            bgColor: "#cccccc",
-            sizes: "(max-width: 690px) 100vw, 690px",
-          },
-        },
-        publishedAt: "2023-05-21T18:48:00+01:00",
-        slug: "test-post-3",
-        content: "Some content",
-        tags: [{ id: "116306017", title: "Front End" }],
-        seo: null,
-      },
-    ],
-    site: {
-      favicon: {
-        responsiveImage: {
-          src: "https://www.datocms-assets.com/101601/1684694095-favicon-5.png?crop=focalpoint&h=64&w=64",
-        },
-      },
-      globalSeo: {
-        facebookPageUrl: null,
-        siteName: "Dan Holloran",
-        titleSuffix: "- Dan Holloran",
-        twitterAccount: "@dholloran",
-        fallbackSeo: {
-          description:
-            "I'm a Full Stack Developer and Photographer based in St. Louis. I love traveling and enjoy mentoring others. Let's create something amazing together!",
-          title: "Dan Holloran",
-          twitterCard: "summary_large_image",
-          image: null,
-        },
-      },
-    },
-  };
-  // const query = `
-  // ${PostAttributes}
-  // ${ResponsiveImageAttributes}
-  // ${SeoAttributes}
-  // ${SiteAttributes}
-  // ${TagAttributes}
-  // {
-  //   posts: allPosts(first: 100) {
-  //     ...PostAttributes
-  //   }
-  //   site: _site {
-  //     ...SiteAttributes
-  //   }
-  // }
-  // `;
+  info("Downloading models from Dato...");
+  const query = `
+  ${PostAttributes}
+  ${ResponsiveImageAttributes}
+  ${SeoAttributes}
+  ${SiteAttributes}
+  ${TagAttributes}
+  {
+    posts: allPosts(first: 100) {
+      ...PostAttributes
+    }
+    site: _site {
+      ...SiteAttributes
+    }
+  }
+  `;
 
-  // return await request({ query });
+  return await request({ query });
 };
 
 const request = async ({ query }) => {
@@ -298,7 +101,7 @@ const request = async ({ query }) => {
     const json = await response.json();
     return json.data;
   } catch (error) {
-    console.log(error.data);
+    danger(error.data);
 
     return null;
   }
