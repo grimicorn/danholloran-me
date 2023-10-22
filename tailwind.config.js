aa79b6424c9853bd0237d5497f12b02d219ce145
Danny Holloran <DTHolloran@Gmail.com>
2023-10-22 16:22:12 -0500

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./src/**/*.{html,js,vue}",
     "./.vitepress/theme/**/*.{html,js,vue}",
-    "./data/**/*.md",
+    "./content/**/*.md",
   ],
   theme: {
     extend: {
--- tina/config.ts
+++ tina/config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   branch,
   clientId: "572b4d1d-6510-4286-9a03-bb80020bbd69", // Get this from tina.io
   token: "c2fefda563ca91c54c19432c7790c5191880b6c1", // Get this from tina.io
-
   build: {
     outputFolder: "admin",
     publicFolder: "public",
@@ -17,6 +16,14 @@ export default defineConfig({
       mediaRoot: "",
       publicFolder: "public",
     },
+  },
+  search: {
+    tina: {
+      indexerToken: '4a8bd2fe559741cdc01c8866ce68fa9fd3a40b88',
+      stopwordLanguages: ['eng']
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
   },
   schema: {
     collections: [
