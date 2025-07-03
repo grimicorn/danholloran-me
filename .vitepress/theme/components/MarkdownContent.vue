20876641583f0e71c9b2f1ea31b211e729ab425a
Danny Holloran <hello@danholloran.me>
2025-07-03 13:45:58 -0400

    wip

---
+++ .vitepress/theme/components/MarkdownContent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { marked } from "marked";
+import { computed } from "vue";
+
+const props = defineProps({
+  content: { type: String, default: "" },
+});
+
+const html = computed(() => marked.parse(props.content));
+</script>
+
+<template>
+  <div v-html="html"></div>
+</template>
