6343e979ba3c0f7e59cbe1fa4b8ceca1fafec2eb
Danny Holloran <hello@danholloran.me>
2025-07-03 07:31:37 -0400

    adds content

---
+++ content/social-links.js
@@ -0,0 +1,5 @@
+export default [
+  { label: `Github`, url: `/social/github/`, icon: `github.svg` },
+  { label: `Instagram`, url: `/social/instagram/`, icon: `instagram.svg` },
+  { label: `LinkedIn`, url: `/social/linkedin/`, icon: `linkedin.svg` },
+];
