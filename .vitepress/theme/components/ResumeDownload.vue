7ef6d8dfcdc9d21218d08de2399e000eb145f471
Danny Holloran <hello@danholloran.me>
2025-09-08 13:59:40 -0500

    wip

--- .vitepress/theme/components/ResumeDownload.vue
+++ .vitepress/theme/components/ResumeDownload.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-
-// https://www.npmjs.com/package/jspdf
+import { ArrowDownTrayIcon } from "@heroicons/vue/16/solid";
+import jspdf from "jspdf-html2canvas";
+import resume from "../../content/resume.ts";
+// import html2pdf from "html2pdf.js";
 
 const props = defineProps<{
   element: HTMLElement;
 }>();
 
-onMounted(() => {
-  console.log(props.element);
-});
+const handleClick = () => {
+  // @todo Write test to make sure download always works
+
+  // html2pdf(props.element, {
+  //   filename: `${resume.firstName}-${resume.lastName}-resume.pdf`,
+  //   pagebreak: { mode: "avoid-all", before: ".page2el" },
+  //   jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  // });
+  jspdf(props.element, {
+    jsPDF: {
+      format: "letter",
+    },
+    output: `./${resume.firstName}-${resume.lastName}-resume.pdf`,
+  });
+};
 </script>
 
 <template>
-  <div>{{ element }}</div>
+  <div class="mb-12 flex justify-end">
+    <button
+      type="button"
+      :disabled="!props.element"
+      class="button"
+      @click="handleClick"
+    >
+      <ArrowDownTrayIcon class="w-auto h-[1em] mr-1" />
+      Download
+    </button>
+  </div>
 </template>
