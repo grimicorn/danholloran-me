4c2ebf6e82d1fd3bcd2f9de08ea1e254521b56e3
Danny Holloran <hello@danholloran.me>
2025-09-09 12:03:35 -0500

    wip

--- .vitepress/theme/components/ResumeDownload.vue
+++ .vitepress/theme/components/ResumeDownload.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { ArrowDownTrayIcon } from "@heroicons/vue/16/solid";
-import jspdf from "jspdf-html2canvas";
 import resume from "../../content/resume.ts";
 import { ref } from "vue";
-// import html2pdf from "html2pdf.js";
 
 const props = defineProps<{
   element: HTMLElement;
@@ -19,17 +17,20 @@ const downloading = ref(false);
 const handleClick = () => {
   emit("start");
   downloading.value = true;
-  // @todo Write test to make sure download always works
-  setTimeout(() => {
-    jspdf(props.element, {
-      jsPDF: {
-        format: [props.element.offsetWidth, props.element.offsetHeight + 1],
-      },
-      output: `./${resume.firstName}-${resume.lastName}-resume.pdf`,
-    });
 
-    downloading.value = false;
-    emit("end");
+  setTimeout(() => {
+    import("jspdf-html2canvas").then((module) => {
+      // @todo Write test to make sure download always works
+      module.default(props.element, {
+        jsPDF: {
+          format: [props.element.offsetWidth, props.element.offsetHeight + 1],
+        },
+        output: `./${resume.firstName}-${resume.lastName}-resume.pdf`,
+      });
+
+      downloading.value = false;
+      emit("end");
+    });
   }, 500);
 };
 </script>
