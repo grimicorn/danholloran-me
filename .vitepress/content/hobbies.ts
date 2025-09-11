266ad74419b649e27f0d2df3b271447ffabb0eaf
Danny Holloran <hello@danholloran.me>
2025-09-11 04:38:26 -0500

    Removes hobbies list

--- .vitepress/content/hobbies.ts
+++
@@ -1,9 +0,0 @@
-export const HOBBIES = [
-  { name: "Photography (Waterfalls & Mountains)" },
-  { name: "Outdoor Adventures" },
-  { name: "Traveling" },
-  { name: "OneWheel & Skateboarding" },
-  { name: "Music (making some but mostly listening)" },
-  { name: "Learning (Coding & Life)" },
-  { name: "Teaching & Mentoring" },
-];
