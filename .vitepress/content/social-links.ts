e80f764192a75b21c9ead52fc759b4d346520e10
Danny Holloran <hello@danholloran.me>
2026-04-18 10:05:20 -0700

    wip

--- .vitepress/content/social-links.ts
+++ .vitepress/content/social-links.ts
@@ -3,7 +3,7 @@ import LinkedinIcon from "../theme/components/LinkedinIcon.vue";
 import InstagramIcon from "../theme/components/InstagramIcon.vue";
 
 export default [
-  { label: `Github`, url: `/social/github/`, icon: GithubIcon },
-  { label: `Instagram`, url: `/social/instagram/`, icon: InstagramIcon },
-  { label: `LinkedIn`, url: `/social/linkedin/`, icon: LinkedinIcon },
+  { label: `Github`, url: `/social/github`, icon: GithubIcon },
+  { label: `Instagram`, url: `/social/instagram`, icon: InstagramIcon },
+  { label: `LinkedIn`, url: `/social/linkedin`, icon: LinkedinIcon },
 ];
