e785f05062745366347a039db65a8ffc703fd3c2
Danny Holloran <hello@danholloran.me>
2026-05-10 15:32:54 -0700

    Adds 404 not found page

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,10 @@
 import type { Theme } from "vitepress";
 import AppLayout from "./AppLayout.vue";
+import NotFoundView from "@views/NotFoundView.vue";
 import "./style.css";
 
 export default {
   Layout: AppLayout,
+  NotFound: NotFoundView,
   enhanceApp() {},
 } satisfies Theme;
