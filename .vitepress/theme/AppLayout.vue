72a265078178f1c7768fb2eb90b65e05dc8db7d3
Danny Holloran <hello@danholloran.me>
2026-05-11 16:58:19 -0700

    Re-enable search, fix uneven pagination

--- .vitepress/theme/AppLayout.vue
+++ .vitepress/theme/AppLayout.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import AppNav from "@components/AppNav.vue";
 import AppFooter from "@components/AppFooter.vue";
-// import AppSearch from "@components/AppSearch.vue";
+import AppSearch from "@components/AppSearch.vue";
 </script>
 
 <template>
   <AppNav />
-  <!-- <AppSearch /> -->
+  <AppSearch />
   <Content />
   <AppFooter />
 </template>
