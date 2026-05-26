ae23322e94c47c5e8aff1184b62e90939b860a44
Danny Holloran <hello@danholloran.me>
2026-05-25 19:07:14 -0500

    Add X social link to nav and mobile menu

--- .vitepress/data/socialLinks.ts
+++ .vitepress/data/socialLinks.ts
@@ -2,4 +2,5 @@ export default Object.freeze({
   GITHUB: "https://github.com/grimicorn",
   INSTAGRAM: "https://instagram.com/grimicornsparkles/",
   LINKEDIN: "https://linkedin.com/in/dan-holloran/",
+  X: "https://x.com/grimicorn",
 });
